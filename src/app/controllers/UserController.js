e84f6fad75bd827c559033f0ee8d2b0878e1fead
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2022-11-20 01:17:39 +0700

    up

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const {mutipleMongooseToObject, mongooseToObject} = require('../../util/mongoose')
 const Book = require('../models/Book')
+const alert = require('alert')
 class UserControllers {
     // [Get] /login
     login(req, res, next) {
@@ -32,6 +33,25 @@ class UserControllers {
         .catch((err)=> {
             console.log('err: ', err)
             alert("Tài khoản không tồn tại")
+        })
+    }
+
+    register(req, res, next) {
+        res.render('register')
+    }
+
+    create(req, res, next) {
+        User.findOne({
+            username: req.body.username,
+            //password: req.body.password,
+        })
+        .then((user)=> {
+            if(user) {
+                alert('Tồn tại tài khoản')
+            }
+            else if(!user) {
+                alert('Có thể đăng ký')
+            }
         })
     }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -6,6 +6,6 @@ const userController = require('../app/controllers/UserController')
 //newsController.index
 router.get('/', userController.login)
 router.post('/auth', userController.auth)
-
-
+router.get('/create', userController.register)
+router.post('/create/auth', userController.create)
 module.exports = router;
