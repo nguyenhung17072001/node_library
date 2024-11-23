c24f58cbf196a3dd88e6f43121b1946e1cb15111
Nguyen Hung <hung.nv@taki.vn>
2024-11-23 11:22:19 +0700

    up

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,7 +4,7 @@ async function connect() {
     try {
         //await mongoose.connect('mongodb://localhost:27017/library');
         //await mongoose.connect('mongodb+srv://nguyenhung17072001:Gj562u5tGX4v1xlA@quang.yuespkr.mongodb.net/library?retryWrites=true&w=majority&appName=persional');
-        //await mongoose.connect('mongodb+srv://quang:Gj562u5tGX4v1xlA@persional.yuespkr.mongodb.net/library?retryWrites=true&w=majority&appName=persional');
+        await mongoose.connect('mongodb+srv://quang:Gj562u5tGX4v1xlA@persional.yuespkr.mongodb.net/library?retryWrites=true&w=majority&appName=persional');
         
 
         console.log('connect successfully!!!! ');
